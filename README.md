72befc6b53eee31c71aed349d577c517b35a4051
Shariff Faleel <shariff.mfa@outlook.com>
2024-08-14 14:07:19 -0700

    Update readme and doc strings

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# skeleton-xr-hand-provider
+# Skeletong XR Hands
+
+This provides a component that feeds the locations of transforms to XR Hands. 
+This is useful if when you would like transforms poses, generally in the form of skeletons that may be controlled by other systems, to be exposed through XR Hands API so that any system that uses the XR Hands API can be used with  the custom skeleton.
+
+To use simply add the `SkeletonXRHands` component anywhere and configure the JointIDs and the corresponding transform whose pose is to be exposed through the XR Hands API.
--- Runtime/SkeletonXRHands.cs
+++ Runtime/SkeletonXRHands.cs
@@ -7,12 +7,19 @@ using UnityEngine.XR.Hands.ProviderImplementation;
 namespace ubco.ovilab.SkeletonXRHandProvider
 {
     [Serializable]
+    /// <summary>
+    /// The container struct that contains the <see cref="XRHandJointID">jointID</see> and <see cref="Transform"/>
+    /// </summary>
     public struct SkeletonKeyPair
     {
         public XRHandJointID jointID;
         public Transform transform;
     }
 
+    /// <summary>
+    /// The component that is used to setup the skeleton hands.
+    /// Only one component is expected to be active at a time.
+    /// </summary>
     public class SkeletonXRHands: MonoBehaviour
     {
         private static bool instanceExists = false;
